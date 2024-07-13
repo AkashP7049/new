6abd51f597f6b73babb57b833154edfa35742b2c
AkashP7049 <akash7049695103gmail.com>
2024-07-13 15:19:36 +0530

    hehe

---
+++ BasicJS/Example.js
@@ -0,0 +1,20 @@
+// console.log('hello');
+
+// var a=5;
+// console.log(a)
+// var a=7;
+// console.log(a);
+// let b=10
+// b=10
+
+//console.log(b);
+// const c=12
+// c=154
+// console.log(c)
+
+let FirstName='Akash'
+let LastName='Patel'
+console.log(`My Firstname is ${FirstName}  My Lastname is ${LastName}`)
+//console.log(FirstName+" "+LastName)
+let a=false
+console.log(a)
---
+++ lecture1js/index.js
@@ -0,0 +1,99 @@
+// var a 
+// console.log(a); // undefined
+
+// var a=null
+// console.log(a) // null datatype
+
+//var arr=[1,2,3,4,5,6,6,'string',true]
+  //  console.log(arr);
+
+//   for(let i in arr){
+//     console.log(i);   // for in print index value
+//   }
+
+//   for(let i of arr){
+//     console.log(i)   // for of print array value
+//   }
+
+
+//CONDITIONAL STATEMENT
+
+// let a=83
+// if(a>90){
+//     console.log('A');
+// }
+// else if(a>=85){
+//     console.log('B')
+// }
+// else if(a>=70){
+//     console.log('C')
+// }
+// else{
+//     console.log('Fail')
+// }
+
+//TRINARY OPERATOR
+//let b=true
+//b?console.log(true):console.log(false);
+
+//AND OPERATOR
+// let a=95
+// let c=20
+// if(a>90&&c==20){
+//     console.log('A');
+// }
+// else if(a>=85){
+//     console.log('B')
+// }
+// else if(a>=70){
+//     console.log('C')
+// }
+// else{
+//     console.log('Fail')
+// }
+
+//OR OPERATOR
+// let a=83
+// let c=20
+// if(a>90 || c==20){
+//     console.log('A');
+// }
+// else if(a>=85){
+//     console.log('B')
+// }
+// else if(a>=70){
+//     console.log('C')
+// }
+// else{
+//     console.log('Fail')
+// }
+
+
+//FUNCTION
+// function add(){
+//     console.log('hellooo');
+// }
+
+// add()
+
+// const add=() => {        //Arrow function
+//     console.log('helloo');  
+// }
+// add()
+
+
+// function add(a,b){
+//     console.log(a+b);
+// }
+// add(5,6)
+
+// function add(a,b,c){
+//         console.log(a+b);
+//         console.log('heiiiii')
+//         return 'helloo'
+//     }
+//     let a=add()
+//     console.log(a)
+
+// let add=(a,b) =>a+b   //paramized/arrow function
+// console.log(add( 15,20))
